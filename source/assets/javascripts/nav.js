85f676ade220f6e5c1e5adf25ed0791fcf05cece
Ross Kaffenberger <rosskaff@gmail.com>
2016-02-27 11:05:34 -0500

    Nav content with scroll js

---
+++ source/assets/javascripts/nav.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+
+  // Variables
+  var $nav = $('.navbar'),
+      $body = $('body'),
+      $window = $(window),
+      navOffsetTop = $nav.offset().top,
+      $document = $(document),
+      entityMap = {
+        "&": "&amp;",
+        "<": "&lt;",
+        ">": "&gt;",
+        '"': '&quot;',
+        "'": '&#39;',
+        "/": '&#x2F;'
+      }
+
+  function init() {
+    $window.on('scroll', onScroll)
+    $window.on('resize', resize)
+    $document.on('click', closePopover)
+    $('a[href^="#"]').on('click', smoothScroll)
+  }
+
+  function smoothScroll(e) {
+    e.preventDefault();
+    $(document).off("scroll");
+    var target = this.hash,
+        menu = target;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top-40
+    }, 0, 'swing', function () {
+        window.location.hash = target;
+        $(document).on("scroll", onScroll);
+    });
+  }
+
+  function resize() {
+    $body.removeClass('has-docked-nav')
+    navOffsetTop = $nav.offset().top
+    onScroll()
+  }
+
+  function onScroll() {
+    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
+      $body.addClass('has-docked-nav')
+    }
+    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
+      $body.removeClass('has-docked-nav')
+    }
+  }
+
+  init();
+});
