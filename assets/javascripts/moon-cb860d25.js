604ecfef98ba1efa03449ce9e23c1fa3889ccd1f
Ross Kaffenberger <rosskaff@gmail.com>
2016-02-27 12:23:28 -0500

    Site updated to 028b4b4

---
+++ assets/javascripts/moon-cb860d25.js
@@ -0,0 +1,1 @@
+"use strict";!function(){var i=document.querySelector("body");i.classList.add("is-loading"),window.addEventListener("load",function(){window.setTimeout(function(){i.classList.remove("is-loading");var e=document.querySelector("#moon div");e.classList.add("visible")},100)})}();
