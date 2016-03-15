4dbe357498b533a52093c01ac93a3d5aca1917c8
Desmond Bowe <desmond@crevalle.io>
2016-03-15 15:27:37 -0400

    Subculture image carousel

---
+++ source/assets/javascripts/carousel.js
@@ -0,0 +1,15 @@
+
+(function() {
+  var $carousel = $('.subculture-carousel');
+
+  $carousel.slick({
+    dots: true,
+    speed: 300,
+    infinite: true,
+    slidesToShow: 3,
+    centerPadding: '10px',
+    focusOnSelect: false
+  });
+
+})();
+
