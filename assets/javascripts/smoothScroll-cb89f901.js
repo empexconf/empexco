861e42bba272b980e4bfe3a24cabc7b8af4685e0
Will Ockelmann-Wagner <will@whentheresawill.net>
2017-12-15 00:40:37 -0800

    Site updated to 3019fd8

---
+++ assets/javascripts/smoothScroll-cb89f901.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$('a[href*="#"]').not('[href="#"]').not('[href="#0"]').click(function(e){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var t=$(this.hash);t=t.length?t:$("[name="+this.hash.slice(1)+"]"),t.length&&(e.preventDefault(),$("html, body").animate({scrollTop:t.offset().top},1e3))}})});
