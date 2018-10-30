375bd76459412bf626883ae740b2a50d0b4b96c8
Will Ockelmann-Wagner <will.ockelmann.wagner@gmail.com>
2018-10-30 01:37:49 -0700

    make landing links work

--- source/assets/javascripts/landing.js
+++ source/assets/javascripts/landing.js
@@ -1,10 +1,18 @@
 (function() {
   function navigateToPage(page) {
     return function() {
-      window.location.href = '/' + page + '.html';
-    }
+      window.location.href = "/" + page + ".html";
+    };
   }
 
-  $('.hero-card--nyc').click(navigateToPage('nyc'));
-  $('.hero-card--la').click(navigateToPage('la'));
+  function allowClick(event) {
+    console.log("allow", event);
+    event.stopPropagation();
+  }
+
+  $(".hero-card--nyc").click(navigateToPage("nyc"));
+  $(".hero-card--la").click(navigateToPage("la"));
+
+  $(".hero-card--nyc a").click(allowClick);
+  $(".hero-card--la a").click(allowClick);
 })();
